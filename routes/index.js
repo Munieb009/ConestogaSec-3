b74f8627d4e3b4052641e9d5eecadcb5b85cbe7c
Munieb1603 <amunieb@gmail.com>
2021-07-19 08:33:49 -0400

    Sec-3 Express

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ProjectTrackerSec3' });
+});
+
+router.get('/about', (req,res,next) => {
+  res.render('about', {title : 'About',
+  pageText: 'Here is some information about this PRoject'
+})
+})
+
+module.exports = router;
